e16ed4e9270493b7bc3df5bb9af7e438aec58a3d
Maryin-c <3317701822@qq.com>
2025-02-17 22:33:51 +0800

    add llama long term ETTh1 336 bash

---
+++ new_bash/long_term_ETTh1_336.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#SBATCH --job-name=time_llm_test
+#SBATCH --output=./res/long_term_ETTh1_336_res.txt
+#SBATCH --error=./res/long_term_ETTh1_336_error.txt
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:h100-47:1
+#SBATCH --mem=64G
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=e1350606@u.nus.edu
+
+source ../../miniconda3/etc/profile.d/conda.sh
+conda activate timellm
+
+model_name=TimeLLM
+train_epochs=10
+learning_rate=0.01
+llama_layers=32
+
+#master_port=25000
+#num_process=2
+batch_size=8
+d_model=32
+d_ff=128
+
+comment='TimeLLM-ETTh1'
+
+#accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+accelerate launch --mixed_precision bf16 run_main.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_512_336 \
+  --model $model_name \
+  --data ETTh1 \
+  --features M \
+  --seq_len 512 \
+  --label_len 48 \
+  --pred_len 336 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --lradj 'COS'\
+  --learning_rate 0.001 \
+  --llm_layers $llama_layers \
+  --train_epochs $train_epochs \
+  --model_comment $comment
