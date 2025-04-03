b06ca220087b0586001ea0ae59dfffc0f3e344d0
Maryin-c <3317701822@qq.com>
2025-04-03 11:03:19 +0800

    add prompt res

--- new_bash/prompt_test.sh
+++ new_bash/prompt_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=prompt
-#SBATCH --output=./res/prompt_res.txt
-#SBATCH --error=./res/prompt_error.txt
+#SBATCH --output=./res/prompt_else_res.txt
+#SBATCH --error=./res/prompt_else_error.txt
 #SBATCH --time=120:00:00
 #SBATCH --gres=gpu:h100-47:1
 #SBATCH --mem=256G
@@ -13,73 +13,73 @@ source ../../miniconda3/etc/profile.d/conda.sh
 conda activate timellm
 
 model_name=TimeLLM
-train_epochs=10
+train_epochs=6
 learning_rate=0.01
 llama_layers=32
 
 #master_port=25000
 #num_process=2
-batch_size=8
+batch_size=2
 d_model=32
 d_ff=128
 
-comment='TimeLLM-ETTh1'
+comment='TimeLLM-ETTh2'
 
 #########################################
-accelerate launch --mixed_precision bf16 run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_512_192 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 192 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model 32 \
-  --d_ff 128 \
-  --batch_size $batch_size \
-  --learning_rate 0.02 \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment \
-  --prompt_domain 1
+#accelerate launch --mixed_precision bf16 run_main.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+#  --root_path ./dataset/ETT-small/ \
+#  --data_path ETTh1.csv \
+#  --model_id ETTh1_512_192 \
+#  --model $model_name \
+#  --data ETTh1 \
+#  --features M \
+#  --seq_len 512 \
+#  --label_len 48 \
+#  --pred_len 192 \
+#  --factor 3 \
+#  --enc_in 7 \
+#  --dec_in 7 \
+#  --c_out 7 \
+#  --des 'Exp' \
+#  --itr 1 \
+#  --d_model 32 \
+#  --d_ff 128 \
+#  --batch_size $batch_size \
+#  --learning_rate 0.02 \
+#  --llm_layers $llama_layers \
+#  --train_epochs $train_epochs \
+#  --model_comment $comment \
+#  --prompt_domain 1
 
 
-accelerate launch --mixed_precision bf16 run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_512_720 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 720 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --d_model $d_model \
-  --d_ff $d_ff \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment \
-  --prompt_domain 1
+#accelerate launch --mixed_precision bf16 run_main.py \
+#  --task_name long_term_forecast \
+#  --is_training 1 \
+# --root_path ./dataset/ETT-small/ \
+#  --data_path ETTh1.csv \
+#  --model_id ETTh1_512_720 \
+#  --model $model_name \
+#  --data ETTh1 \
+#  --features M \
+#  --seq_len 512 \
+#  --label_len 48 \
+#  --pred_len 720 \
+#  --factor 3 \
+#  --enc_in 7 \
+#  --dec_in 7 \
+#  --c_out 7 \
+#  --des 'Exp' \
+#  --itr 1 \
+#  --d_model $d_model \
+#  --d_ff $d_ff \
+#  --batch_size $batch_size \
+#  --learning_rate $learning_rate \
+#  --llm_layers $llama_layers \
+#  --train_epochs $train_epochs \
+#  --model_comment $comment \
+#  --prompt_domain 1
 ######################################
 
 
@@ -143,7 +143,7 @@ accelerate launch --mixed_precision bf16 run_main.py \
   --learning_rate 0.005 \
   --lradj 'TST'\
   --llm_layers $llama_layers \
-  --train_epochs 20 \
+  --train_epochs $train_epochs \
   --patience 10 \
   --model_comment $comment \
   --prompt_domain 1
